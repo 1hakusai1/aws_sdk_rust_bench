4c2062bf9745512eb5b520ad4a3e4510223adffd
1hakusai1 <1hakusai1@gmail.com>
2025-01-24 15:59:13 +0900

    List bucket contents using aws_sdk_s3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error::Error};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut args = env::args();
+    let bucket = args.nth(1).ok_or("Specify a bucket name")?;
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_s3::Client::new(&config);
+    let result = client.list_objects().bucket(bucket).send().await?;
+    let file_names = result
+        .contents()
+        .iter()
+        .filter_map(|o| o.key())
+        .collect::<Vec<_>>();
+    dbg!(file_names);
+    Ok(())
 }
