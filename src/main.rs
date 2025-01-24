cd955c8b078e62e2952a5c0ae1397b9590651257
1hakusai1 <1hakusai1@gmail.com>
2025-01-24 22:54:25 +0900

    Print response itself

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_s3::Client::new(&config);
     let result = client.list_objects().bucket(bucket).send().await?;
-    let file_names = result
-        .contents()
-        .iter()
-        .filter_map(|o| o.key())
-        .collect::<Vec<_>>();
-    println!("{:?}", file_names);
+    println!("{:?}", result);
     Ok(())
 }
