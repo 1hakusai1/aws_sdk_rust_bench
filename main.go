8b257273dfd4cd1c75b0f145f772a1c195a9d06a
1hakusai1 <1hakusai1@gmail.com>
2025-01-27 19:52:09 +0900

    Add go implementatioin

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/smithy-go"
+)
+
+// main uses the AWS SDK for Go V2 to create an Amazon Simple Storage Service
+// (Amazon S3) client and list up to 10 buckets in your account.
+// This example uses the default settings specified in your shared credentials
+// and config files.
+func main() {
+	start := time.Now()
+
+	bucket := os.Args[1]
+	ctx := context.Background()
+	sdkConfig, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
+		fmt.Println(err)
+		return
+	}
+	s3Client := s3.NewFromConfig(sdkConfig)
+	result, err := s3Client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{Bucket: &bucket})
+	if err != nil {
+		var ae smithy.APIError
+		if errors.As(err, &ae) && ae.ErrorCode() == "AccessDenied" {
+			fmt.Println("You don't have permission to list buckets for this account.")
+		} else {
+			fmt.Printf("Couldn't list buckets for your account. Here's why: %v\n", err)
+		}
+		return
+	}
+	fmt.Println(result)
+	elapsed := time.Since(start)
+	fmt.Printf("Binomial took %s", elapsed)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::{env, error::Error};
+use std::{env, error::Error, time::Instant};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let start = Instant::now();
     let mut args = env::args();
     let bucket = args.nth(1).ok_or("Specify a bucket name")?;
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_s3::Client::new(&config);
     let result = client.list_objects_v2().bucket(bucket).send().await?;
     println!("{:?}", result);
+    let elapsed = start.elapsed();
+    println!("{:?}", elapsed);
     Ok(())
 }
